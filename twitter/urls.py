97479e37327c4e6edba8a8937cfbce50f99e48f3
Imaralievi4 <zkd3year@gmail.com>
2021-01-07 18:54:08 +0600

    second commit

--- twitter/urls.py
+++ twitter/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+from .views import redirect_blog
+
 
 urlpatterns = [
+    path('', redirect_blog),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
 ]
